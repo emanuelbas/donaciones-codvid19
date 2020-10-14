de10579c6a814662c7f8b1d291abace0673e389d
emanuelbas <emanuelbastons@gmail.com>
2020-10-13 21:45:57 -0300

    Agrego roles al modelo

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -11,3 +11,17 @@ class User(db.Model):
 
     def all():
         return User.query.all()
+
+    #Relaciones
+    roles = db.relationship('Role', secondary='user_roles')
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), unique=True)
+
+class UserRoles(db.Model):
+	__tablename__ = 'user_roles'
+	id = db.Column(db.Integer(), primary_key=True)
+	user_id = db.Column(bd.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+	role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
