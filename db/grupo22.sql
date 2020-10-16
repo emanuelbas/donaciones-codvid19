3db5a275977cade49b75dd85c6a260a345c0c5e1
Mayrukia <mayrukia@gmail.com>
2020-10-15 22:17:04 -0300

    creacion del modelo de datos para el modulo

--- db/grupo22.sql
+++ db/grupo22.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-10-2020 a las 16:16:17
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.2.33
+-- Tiempo de generación: 16-10-2020 a las 03:03:00
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -46,23 +47,100 @@ INSERT INTO `configuracion` (`id`, `titulo`, `descripcion`, `mail`, `activo`, `c
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `permiso`
+--
+
+CREATE TABLE `permiso` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `permiso`
+--
+
+INSERT INTO `permiso` (`id`, `nombre`) VALUES
+(1, 'centro_index'),
+(2, 'centro_new'),
+(3, 'centro_destroy'),
+(4, 'centro_update'),
+(5, 'centro_show');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id`, `nombre`) VALUES
+(1, 'admin'),
+(2, 'operador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol_tiene_permiso`
+--
+
+CREATE TABLE `rol_tiene_permiso` (
+  `rol_id` int(10) NOT NULL,
+  `permiso_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
 CREATE TABLE `usuario` (
   `id` int(10) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `clave` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `usuario` varchar(255) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `fecha_actualizacion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `usuario`, `clave`) VALUES
-(1, 'admin', '1234'),
-(2, 'hugo', '123'),
-(3, 'may', '1234');
+INSERT INTO `usuario` (`id`, `usuario`, `clave`, `nombre`, `apellido`, `email`, `activo`, `fecha_creacion`, `fecha_actualizacion`) VALUES
+(1, 'admin', '1234', 'administrador', 'administrador', 'admin@admin.com', 0, '0000-00-00', '0000-00-00'),
+(2, 'may', '1234', 'mayra', 'luengo', 'may@amail.com', 0, '0000-00-00', '0000-00-00'),
+(3, 'dario', '1234', 'Dario', 'Contrera', 'dario@mail.com', 0, '0000-00-00', '0000-00-00'),
+(4, 'ema', '1234', 'enmanuel', 'Bastons', 'ema@ema.com', 0, '0000-00-00', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_tiene_rol`
+--
+
+CREATE TABLE `usuario_tiene_rol` (
+  `usuario_id` int(10) NOT NULL,
+  `rol_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuario_tiene_rol`
+--
+
+INSERT INTO `usuario_tiene_rol` (`usuario_id`, `rol_id`) VALUES
+(1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -75,10 +153,30 @@ ALTER TABLE `configuracion`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `permiso`
+--
+ALTER TABLE `permiso`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indices de la tabla `rol_tiene_permiso`
+--
+ALTER TABLE `rol_tiene_permiso`
+  ADD PRIMARY KEY (`rol_id`,`permiso_id`),
+  ADD KEY `FK_permiso_id` (`permiso_id`);
+
+--
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario_tiene_rol`
+--
+ALTER TABLE `usuario_tiene_rol`
+  ADD PRIMARY KEY (`usuario_id`,`rol_id`),
+  ADD KEY `FK_rol_utp_id` (`rol_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -89,12 +187,6 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `configuracion`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
