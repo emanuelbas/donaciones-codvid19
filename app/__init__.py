717c094ef9595c5960b4bac66a9113267997ade6
emanuelbas <emanuelbastons@gmail.com>
2020-10-18 02:55:55 -0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -8,55 +8,58 @@ from app.resources import user
 from app.resources import config
 #from flask_mysqldb import MySQL
 
-# Configuración inicial de la app
-app = Flask(__name__)
-app.config.from_object(Config)
-# session
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
+def create_app():
+	# Configuración inicial de la app
+	app = Flask(__name__)
+	app.config.from_object(Config)
+	# session
+	app.config['SESSION_TYPE'] = 'filesystem'
+	Session(app)
 
-# config db
-app.secret_key = 'hola'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://" + \
-    Config.DB_USER+":"+Config.DB_PASS+"@"+Config.DB_HOST+"/"+Config.DB_NAME
-db.init_app(app)
-
-
-# session agregado por maxi
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
-
-# Autenticacion agregado por maxi
-app.add_url_rule('/login', 'login', user.login)
-app.add_url_rule('/backend', 'user_backend', user.backend, methods=['POST'])
-app.add_url_rule('/logout', 'logout', user.logout)
+	# config db
+	app.secret_key = 'hola'
+	app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://" + \
+	    Config.DB_USER+":"+Config.DB_PASS+"@"+Config.DB_HOST+"/"+Config.DB_NAME
+	db.init_app(app)
 
 
-# ruta a quienes somos
-app.add_url_rule('/quienesomos', 'quienesomos',
-                 user.quienesomos, methods=["POST", "GET"])
+	# session agregado por maxi
+	app.config['SESSION_TYPE'] = 'filesystem'
+	Session(app)
 
-# ruta a centros
-app.add_url_rule('/centros', 'centros', user.centros, methods=["POST", "GET"])
-
-
-# ruta a login
-app.add_url_rule('/login', 'login', user.login)
-
-# CONFIGURACION
-app.add_url_rule('/configuracion/vista_configuracion', 'vista_configuracion',
-                 config.vista_configuracion, methods=["POST", "GET"])
-# usuario
-app.add_url_rule('/usuario/edit_usuario/<id>', 'edit_usuario', user.edit_usuario, methods=['POST', 'GET'])
-app.add_url_rule('/usuario/index_usuario', 'index_usuario', user.index_usuario, methods=["POST", "GET"])
-app.add_url_rule('/usuario/crear_usuario', 'crear_usuario', user.crear_usuario, methods=["POST", "GET"])
-app.add_url_rule("/usuarios/borrar_usuario/<id>", 'borrar_usuario', user.borrar, methods=['GET'])
-
-# ruta al backend
-app.add_url_rule('/backend', 'backend', user.backend, methods=["POST", "GET"])
+	# Autenticacion agregado por maxi
+	app.add_url_rule('/login', 'login', user.login)
+	app.add_url_rule('/backend', 'user_backend', user.backend, methods=['POST'])
+	app.add_url_rule('/logout', 'logout', user.logout)
 
 
-# index
-@app.route('/')
-def index():
-    return render_template('index.html')
+	# ruta a quienes somos
+	app.add_url_rule('/quienesomos', 'quienesomos',
+	                 user.quienesomos, methods=["POST", "GET"])
+
+	# ruta a centros
+	app.add_url_rule('/centros', 'centros', user.centros, methods=["POST", "GET"])
+
+
+	# ruta a login
+	app.add_url_rule('/login', 'login', user.login)
+
+	# CONFIGURACION
+	app.add_url_rule('/configuracion/vista_configuracion', 'vista_configuracion',
+	                 config.vista_configuracion, methods=["POST", "GET"])
+	# usuario
+	app.add_url_rule('/usuario/edit_usuario/<id>', 'edit_usuario', user.edit_usuario, methods=['POST', 'GET'])
+	app.add_url_rule('/usuario/index_usuario', 'index_usuario', user.index_usuario, methods=["POST", "GET"])
+	app.add_url_rule('/usuario/crear_usuario', 'crear_usuario', user.crear_usuario, methods=["POST", "GET"])
+	app.add_url_rule("/usuarios/borrar_usuario/<id>", 'borrar_usuario', user.borrar, methods=['GET'])
+
+	# ruta al backend
+	app.add_url_rule('/backend', 'backend', user.backend, methods=["POST", "GET"])
+
+
+	# index
+	@app.route('/')
+	def index():
+	    return render_template('index.html')
+	    
+	return app
