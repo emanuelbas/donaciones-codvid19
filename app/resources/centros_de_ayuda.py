feaeec63eb56fa29ff2086e8ba2cbacae1e0d15f
emanuelbas <emanuelbastons@gmail.com>
2020-11-03 20:00:40 -0300

    Agrego checkboxes con tipo al alta de centro

--- app/resources/centros_de_ayuda.py
+++ app/resources/centros_de_ayuda.py
@@ -39,10 +39,11 @@ def crear_centro():
 	if request.method == "POST":
 		form = request.form
 		if Centro_de_ayuda.existe_nombre(form['municipio'],form['nombre']):
+			print(form.getlist("tipos"))
 			mensaje_error="Ya existe un centro con ese nombre para el municipio indicado"
 			return render_template('centro_de_ayuda/crear_centro.html', mensaje_error= mensaje_error, mensaje_exito=mensaje_exito)
 		else:
-			lista_de_tipos = []
+			
 			res = Centro_de_ayuda.crear(
 				protocolo='PDF',
 				historico=0,
@@ -55,7 +56,7 @@ def crear_centro():
 				email=form['email'],
 				corx=form['corx'],
 				cory=form['cory'],
-				lista_de_tipos=lista_de_tipos,
+				lista_de_tipos=form.getlist("tipos"),
 				id_municipio=form['municipio'],
 				id_estado=1)
 			if res:
@@ -65,7 +66,8 @@ def crear_centro():
 			return render_template('index_centros.html', mensaje_error= mensaje_error, mensaje_exito=mensaje_exito)
 	else:
 		lista_de_municipios = obtener_dic_de_municipios()
-		return render_template('centro_de_ayuda/crear_centro.html', municipios=lista_de_municipios, mensaje_error= mensaje_error, mensaje_exito=mensaje_exito)
+		lista_de_tipos = Tipo_de_centro.all()
+		return render_template('centro_de_ayuda/crear_centro.html', tipos= lista_de_tipos, municipios=lista_de_municipios, mensaje_error= mensaje_error, mensaje_exito=mensaje_exito)
 
 
 def obtener_municipios(lista):
