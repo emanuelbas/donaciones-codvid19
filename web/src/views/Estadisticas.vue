14dec770423692706cca020a57e215903808cec6
emanuelbas <emanuelbastons@gmail.com>
2020-12-19 01:30:34 -0300

    Instalacion de v-charts

---
+++ web/src/views/Estadisticas.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Title title="Estadísticas"/>
+    <ve-line :data="chartData"></ve-line>
+  </div>
+</template>
+
+<script>
+import VeLine from 'v-charts/lib/line.common';
+//import axios from "axios";
+import Title from '@/components/Title.vue';
+export default {
+  name: 'Estadisticas',
+  components: { VeLine, Title },
+  data () {
+    return {
+      chartData: {
+        columns: ['date', 'PV'],
+        rows: [
+          { 'date': '01-01', 'PV': 1231 },
+          { 'date': '01-02', 'PV': 1223 },
+          { 'date': '01-03', 'PV': 2123 },
+          { 'date': '01-04', 'PV': 4123 },
+          { 'date': '01-05', 'PV': 3123 },
+          { 'date': '01-06', 'PV': 7123 }
+        ]
+      }
+    }
+  }
+}
+</script>
