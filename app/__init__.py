ae696300368f657d703f125d009a8d3e968b22f6
emanuelbas <emanuelbastons@gmail.com>
2021-02-09 23:45:33 -0300

    Solucionando CORS

--- app/__init__.py
+++ app/__init__.py
@@ -15,12 +15,12 @@ from requests import get
 from app.resources import api
 from app.resources.Api import centros
 from app.resources.Api import turnos
-#from flask_cors import CORS
+from flask_cors import CORS
 
 def create_app():
     # Configuración inicial de la app
     app = Flask(__name__)
-    #cors = CORS(app, resources={r"/api/*": {"origins": "https://grupo22.proyecto2020.linti.unlp.edu.ar/"}}, support_credentials=True)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.config.from_object(Config)
     fa = FontAwesome(app)
     app.config['SESSION_TYPE'] = 'filesystem'
